160de78f8a5cd088b37ecfeb70852d52d84184c1
vijayb <upsrtcdev@trimax.in>
2019-01-22 15:30:11 +0530

    Add Time And Date

--- src/main/java/com/example/demo/DiscoveryServerApplication.java
+++ src/main/java/com/example/demo/DiscoveryServerApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -15,6 +16,7 @@ public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServerApplication.class, args);
+                System.out.println("Current Date And Time in IST : " +new Date());
 	}
 
 }
