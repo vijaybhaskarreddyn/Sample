5d0d789d16c9e6e5c8897f9db134d55a317375e7
vijayb <upsrtcdev@trimax.in>
2019-01-22 15:03:14 +0530

    New Commit

---
+++ src/main/java/com/example/demo/DiscoveryServerApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *
+ * @author root
+ */
+@EnableEurekaServer
+@SpringBootApplication
+
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
+
