cd1e981164078e072b7fd9daf52f2e83a774b48d
VijayReddy <VijayReddy@VijayReddy-PC>
2019-01-26 13:17:14 +0530

    Add New  Class

---
+++ src/main/java/com/example/demo/Demo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+/**
+ *
+ * @author VijayReddy
+ */
+public class Demo {
+    public void checkEvenOROdd(){
+     int n=123;
+    if(n/2==0){
+            System.out.println("The Given Number " +n+ " even");
+}   else if(n/2!=0){
+        System.out.println("The Given Number " +n+ " odd");
+}else{
+        System.out.println("The Given Number " + n +" is invalid");
+}
+    }
+    
+}
